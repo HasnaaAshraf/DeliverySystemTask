20dc16127a391e80d6ab8dacc0068ca03056a708
Hasnaa <167640126+HasnaaAshraf@users.noreply.github.com>
2025-06-10 04:43:38 +0300

    Add project files.

---
+++ DeliverySystem.DAL/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliverySystem.DAL.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        private string Name { get; set; }
+
+        private ProductType Type { get; set; }
+    }
+}
